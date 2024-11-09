7f12a986b6b833f64f2ac4520607abe6186b73f3
ihab-younis <ihab.younis86@gmail.com>
2024-11-09 16:44:55 -0600

    Initiate Spring boot app for customer service

---
+++ src/main/java/com/ihab/interview/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ihab.interview.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ihab/interview/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihab.interview.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
